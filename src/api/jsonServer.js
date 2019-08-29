9dd62d5dc5fd48eb93f0ef19a722f01ac238ea89
claytonschoonoversmith@gmail.com <claytonsmith@Claytons-MacBook-Pro.local>
2019-08-29 00:26:33 -0500

    edit context function rewritten w/ json api - app complete

---
+++ src/api/jsonServer.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+
+export default axios.create({
+	baseURL: 'http://64ab8bab.ngrok.io'
+});
--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,30 +1,38 @@
 import createDataContext from './createDataContext';
+import jsonServer from '../api/jsonServer';
 
 const blogReducer = (state, action) => {
 	switch (action.type) {
+
+		case 'get_blogposts': 
+			return action.payload;
+
 		case 'edit_blogpost':
 			return state.map((blogPost) => {
 				return blogPost.id === action.payload.id ? action.payload : blogPost;
 			});
+
 		case 'delete_blogpost':
-		return state.filter((blogPost) => blogPost.id !== action.payload)
-		case 'add_blogpost': 
-			return [
-				...state, 
-				{ 
-					id: Math.floor(Math.random() * 99999), 
-					title: action.payload.title,
-					cotent: action.payload.content
-				}
-			];
+			return state.filter((blogPost) => blogPost.id !== action.payload);
+
 		default:
 			return state;
 	};
 };
 
+const getBlogPosts = runMyReducer => {
+	return async () => {
+		const response = await jsonServer.get('/blogposts');
+		runMyReducer({ type: 'get_blogposts', payload: response.data });
+										// response.data === [{}, {}, {}]
+	};
+	
+};
+
 const addBlogPost = runMyReducer => {
-	return (title, content, callback) => {
-		runMyReducer({ type: 'add_blogpost', payload: { title, content } });
+	return async (title, content, callback) => {
+		await jsonServer.post('/blogposts', { title, content });
+
 		if(callback) {
 			callback();
 		}
@@ -32,17 +40,22 @@ const addBlogPost = runMyReducer => {
 };
 
 const deleteBlogPost = runMyReducer => {
-	return (id) => {
+	return async (id) => {
+		await jsonServer.delete(`/blogposts/${id}`);
+
 		runMyReducer({ type: 'delete_blogpost', payload: id })
 	};
 }
 
 const editBlogPost = runMyReducer => {
-	return (id, title, content, callback) => {
+	return async (id, title, content, callback) => {
+		await jsonServer.put(`/blogposts/${id}`, { title, content });
+
 		runMyReducer({ 
 			type: 'edit_blogpost', 
 			payload: { id, title, content }
 		});
+
 		if(callback) {
 			callback();
 		}
@@ -51,8 +64,8 @@ const editBlogPost = runMyReducer => {
 
 export const { Context, Provider } = createDataContext(
 	blogReducer, 
-	{ addBlogPost, deleteBlogPost, editBlogPost }, 
-	[{ title: 'TEST POST', content: 'TEST CONTENT', id: 1 }]
+	{ addBlogPost, deleteBlogPost, editBlogPost, getBlogPosts }, 
+	[]
 );
 
 //b/c app is wrapped in a custom component (BlogContext.Provider), 
